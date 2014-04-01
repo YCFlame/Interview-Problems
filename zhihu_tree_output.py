977b42683e6e0b8cdc427e1036e104009a30d80c
yangchao <yangchao@admaster.com.cn>
2014-04-01 18:45:17 +0800

    Debut

---
+++ zhihu_tree_output.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python
+# coding: utf-8
+
+def tree():
+    result = {}
+    data = [
+        'a b c',
+        'b d e',
+        'c f',
+        'd g'
+    ]
+    for line in data:
+        temp = line.split()
+        result[temp[0]] = temp[1:]
+
+    final = [('a', 0)]
+    while final:
+        temp = final.pop(0)
+        if temp[0] in result:
+            x = []
+            for i in result[temp[0]]:
+                x.append((i, temp[1] + 1))
+            final = x + final
+
+        print '\t' * temp[1] + temp[0]
+
+
+if __name__ == '__main__':
+    tree()
