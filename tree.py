23e133a62953ce912088898e72598f10c68a3c19
yangchao <yangchao@admaster.com.cn>
2014-04-11 14:26:48 +0800

    tree structure

---
+++ tree.py
@@ -0,0 +1,49 @@
+#! /usr/bin/env python
+# coding: utf-8
+
+
+class Tree(object):
+
+    def __init__(self, data, left=None, right=None):
+        self.data = data
+        self.left = left
+        self.right = right
+
+
+def build_tree_from_array(array):
+    pass
+
+
+def build_tree_interacitve():
+    pass
+
+
+def build_tree_pre_infix(pre_order, infix_order):
+    pass
+
+
+def build_tree_pre_post(pre_order, post_order):
+    pass
+
+
+def build_tree_infix_post(infix_order, post_order):
+    pass
+
+
+def pre_order_traverse(root):
+    pass
+
+
+def infix_order_traverse(root):
+    pass
+
+
+def post_order_traverse(root):
+    pass
+
+
+def breadth_first_traverse(root):
+    pass
+
+if __name__ == '__main__':
+    pass
