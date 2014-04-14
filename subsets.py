bcac2b34c76005b3691ea62f97da3c23e6b98c1d
YCFlame <yangchao.cs@gmail.com>
2014-04-14 15:56:38 +0800

    add empty set to result

--- subsets.py
+++ subsets.py
@@ -4,10 +4,19 @@
 
 import itertools
 
-if __name__ == '__main__':
-    l = raw_input()
-    l = l.split()
+
+def lasy_combination(l):
     length = len(l)
+    print '()'
     for i in range(1, length + 1):
         for c in itertools.combinations(l, i):
             print c
+
+
+def combination(l):
+    pass
+
+if __name__ == '__main__':
+    l = raw_input()
+    l = l.split()
+    lasy_combination(l)
