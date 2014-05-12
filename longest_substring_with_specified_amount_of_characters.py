7e48bbd217ef8303b6fa33d817586306fa2f6b48
YCFlame <yangchao.cs@gmail.com>
2014-05-12 23:30:21 +0800

    fix bug

--- longest_substring_with_specified_amount_of_characters.py
+++ longest_substring_with_specified_amount_of_characters.py
@@ -3,30 +3,31 @@
 
 
 def check(data, n):
-    appear = set()
+    appear = {}
     start = end = 0
     result = ""
-    next_start = start + 1
     while end < len(data):
         if data[end] not in appear:
             if len(appear) == n:
                 if (end - start + 1) > len(result):
                     result = data[start:end]
 
-                start = next_start
-                next_start = start + 1
-                appear = set(data[start:end])
+                while len(appear) == n:
+                    appear[data[start]] -= 1
+                    if appear[data[start]] == 0:
+                        appear.pop(data[start])
+                    start += 1
 
-            appear.add(data[end])
+            appear[data[end]] = 1
         else:
-            # 如果遇到重复字符，下一次 start 跳转的时候就应该跳转到最后一个重复字符的地方，因为即使从中间开始也不会更长
-            next_start = end
+            appear[data[end]] += 1
 
         end += 1
+
     return result
 
 
 if __name__ == '__main__':
-    data = "klsdjfowejlsjshdfjlsjf"
+    data = "abbcabcdkjfelk"
     print data
     print check(data, 4)
