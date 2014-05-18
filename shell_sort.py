5644827c51e4407635d0bd3aa6dbad225a1dcad3
YCFlame <yangchao.cs@gmail.com>
2014-05-18 17:49:34 +0800

    fix bug

--- shell_sort.py
+++ shell_sort.py
@@ -1,21 +1,26 @@
 #! /usr/bin/env python
 # coding: utf-8
 
+from binary_search import first_match
+
 
 def shell_sort(array, step_li):
     length = len(array)
     for step in step_li:
         for i in range(step):
             start = i
-            while start + step < length:
-                if array[start] > array[start + step]:
-                    array[start], array[start + step] = array[start + step], array[start]
-
+            temp = []
+            while start < length:
+                index = first_match(temp, array[start])
+                if index < 0:
+                    index = -index - 1
+                temp.insert(index, array[start])
                 start += step
+            array[i::step] = temp
 
     return array
 
 if __name__ == '__main__':
     step_li = [4, 2, 1]
-    array = [9, 7, 8, 6, 2, 1, 3, 5, 4]
+    array = [9, 7, 8, 6, 2, 1, 3, 5, 4, 10, 0]
     print shell_sort(array, step_li)
