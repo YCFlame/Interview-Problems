2eda15483353671e836238c125eff166fbe0facc
YCFlame <yangchao.cs@gmail.com>
2014-05-19 11:44:16 +0800

    test case for __getattr__

---
+++ call_chain_to_path.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+# coding: utf-8
+
+
+class Chain(object):
+    def __init__(self, path=''):
+        self._path = path
+
+    def __getattr__(self, path):
+        return Chain('%s/%s' % (self._path, path))
+
+    def users(self, name):
+        return Chain('%s/users/%s' % (self._path, name))
+
+    def __str__(self):
+        return self._path
+
+if __name__ == '__main__':
+    print Chain().users('michael').repos
