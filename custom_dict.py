403ff65a95e4320ef21b7f1e93c1457b36b523a9
YCFlame <yangchao.cs@gmail.com>
2014-05-27 12:14:26 +0800

    add __init__

--- custom_dict.py
+++ custom_dict.py
@@ -4,6 +4,10 @@
 
 class LowerKey(dict):
 
+    def __init__(self, **kwargs):
+        kwargs = {k.lower(): v for k, v in kwargs.items()}
+        super(LowerKey, self).__init__(**kwargs)
+
     def __getitem__(self, key):
         key = key.lower()
         return super(LowerKey, self).__getitem__(key)
@@ -13,7 +17,7 @@ class LowerKey(dict):
         super(LowerKey, self).__setitem__(key, value)
 
 if __name__ == '__main__':
-    x = LowerKey()
+    x = LowerKey(Chao='wang')
     x['YAng'] = "chao"
     print x
     print x['yAng']
